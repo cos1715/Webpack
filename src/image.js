f1fa1eca8cccd3a94bd50c1fd11cc3ba04b0d2de
Taras <tsamoilenko97@gmail.com>
2021-11-08 22:28:36 +0200

    cleared code

--- src/image.js
+++ src/image.js
@@ -1,10 +1,10 @@
-
+import ME from "./assets/tez.png";
 
 export const addImage = () => {
   const node = document.createElement("img");
   node.setAttribute("alt", "OMG");
   node.setAttribute("width", "300");
-  node.setAttribute("src", 'ME');
+  node.setAttribute("src", ME);
   const body = document.getElementById("root");
 
   body.appendChild(node);
